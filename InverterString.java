f7d9d46eef39a2fd5544da076794b23a549679a6
naaaathanferreira <149843378+naaaathanferreira@users.noreply.github.com>
2024-03-08 14:49:52 -0300

    Add files via upload

---
+++ InverterString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class InverterString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma palavra: ");
+        String entrada = scanner.nextLine();
+
+        String palavraInvertida = inverterString(entrada);
+        System.out.println("String invertida: " + palavraInvertida);
+    }
+
+    public static String inverterString(String str) {
+        String invertida = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            invertida += str.charAt(i);
+        }
+        return invertida;
+    }
+}
